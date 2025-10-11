12f597552d149d2d2e2619b91729ea1e7ba94957
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-10-11 22:58:50 +0600

    feat(auth): implement CheckAuth component with role-based redirection and add demo for logic testing
    - Added demo user data to test authentication logic
    
    - Included fallback routes for unauth

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Adminlayout from "./components/admin-view/layout";
 import Authlayout from "./components/auth/layout";
+import CheckAuth from "./components/common/check-auth";
 import UserLayout from "./components/user-view/layout";
 import AdminDashboard from "./pages/admin-view/dashboard";
 import AdminFeatures from "./pages/admin-view/features";
@@ -13,30 +14,59 @@ import UserAccount from "./pages/user-view/account";
 import UserCheckout from "./pages/user-view/checkout";
 import UserHome from "./pages/user-view/home";
 import UserListing from "./pages/user-view/listing";
+import UnauthPage from "./pages/unauth-page/unauth-page";
 
 function App() {
+  const isAuthenticated = true;
+  const user = {
+    name:'Tamim',
+    role:"admin"
+
+  };
   return (
     <>
       <div className="flex flex-col overflow-hidden bg-white">
         <Routes>
-          <Route path="/auth" element={<Authlayout />}>
+          <Route
+            path="/auth"
+            element={
+              <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                <Authlayout />
+              </CheckAuth>
+            }
+          >
             <Route path="login" element={<AuthLogin />} />
             <Route path="register" element={<AuthRegister />} />
           </Route>
 
-          <Route path="/admin" element={<Adminlayout />}>
+          <Route
+            path="/admin"
+            element={
+              <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                <Adminlayout />
+              </CheckAuth>
+            }
+          >
             <Route path="dashboard" element={<AdminDashboard />} />
             <Route path="products" element={<AdminProducts />} />
             <Route path="orders" element={<AdminOrders />} />
             <Route path="features" element={<AdminFeatures />} />
           </Route>
-          <Route path="/user" element={<UserLayout />}> 
+          <Route
+            path="/user"
+            element={
+              <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                <UserLayout />
+              </CheckAuth>
+            }
+          >
             <Route path="home" element={<UserHome />} />
             <Route path="listing" element={<UserListing />} />
             <Route path="checkout" element={<UserCheckout />} />
 
-            <Route path="account" element={<UserAccount  />} />
+            <Route path="account" element={<UserAccount />} />
           </Route>
+          <Route path="/unauth-page" element={<UnauthPage/>}/>
 
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/unauth-page/unauth-page.jsx
@@ -0,0 +1,5 @@
+const UnauthPage = () => {
+  return <h1>You don't have acces to this page</h1>;
+};
+
+export default UnauthPage;
